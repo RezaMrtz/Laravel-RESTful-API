c4ee860ea42a38218097753ef42303450ba4c3cc
Reza <reza.mrtz@gmail.com>
2019-09-20 02:19:24 +0430

    Resolving buyer queries using global scopes

--- app/Buyer.php
+++ app/Buyer.php
@@ -1,12 +1,23 @@
 <?php
 
 namespace App;
+
 use App\Transaction;
+use App\Scopes\BuyerScope;
 
 class Buyer extends User
 {
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new BuyerScope);
+    }
+
+
     public function transactions()
     {
-       return $this->hasMany(Transaction::class);
+        return $this->hasMany(Transaction::class);
     }
 }
