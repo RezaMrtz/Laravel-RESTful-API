1d8a56d5deec8b1bf36df59694d678a73a232d01
Reza <reza.mrtz@gmail.com>
2019-09-29 01:21:21 +0330

    Laravel markdown mailable

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,4 +1,15 @@
+@component('mail::message')
+# Introduction
 Hello {{ $user->name }}
 
-You changed your email, so we need you to confirm your new address. Please use the link below:
-{{ route('verify', $user->verification_token) }}
+You changed your email, so we need you to confirm your new address. Please use the button below:
+
+@component('mail::button', ['url' =>route('verify', $user->verification_token)])
+Verify Account
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
+
+
--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,4 +1,18 @@
-Hello {{$user->name}}
+@component('mail::message')
+# Introduction
+Hello {{ $user->name }}
 
-Thank you for creaete an account. Please verify your email using this link:
-{{ route('verify', $user->verification_token) }}
+Thank you for creaete an account. Please verify your email using this button:
+
+
+@component('mail::button', ['url' =>route('verify', $user->verification_token)])
+Verify Account
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
+
+
+
+
