ebfd26b2b25f680bed3b118b7b4ef75bf55d09aa
RezaMrtz <reza.mrtz@gmail.com>
2019-10-13 12:19:29 +0330

    Some improvements on passport

--- routes/api.php
+++ routes/api.php
@@ -48,5 +48,3 @@ Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['i
 Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
 Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
 Route::resource('sellers.products', 'Seller\SellerProductController' ,['except' => ['create', 'edit']]);
-
-
--- routes/web.php
+++ routes/web.php
@@ -11,29 +11,23 @@
 |
 */
 
-/* Authentication Route */
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+/* Authentication */
+Route::post('/login', 'Auth\LoginController@login');
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 
-Route::get('login', 'Auth\LoginController@login');
-
-Route::get('logout', 'Auth\LoginController@logout')->name('logout');
-
-/* Password Reset Routes */
+/* Password Reset */
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-
 Route::get('password/reset/{token}', 'Auth\ForgotPasswordController@showResetForm')->name('password.reset');
-
 Route::get('password.reset', 'Auth\ResetPasswordController@reset');
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
+/* Home */
+Route::get('/', function () { return view('welcome'); })->middleware('guest');
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/', function () {return view('welcome');})->middleware('guest');;
+/* Passport */
+Route::get('/home/my-tokens', 'HomeController@getTokens')->name('personal-tokens');
+Route::get('/home/my-clients', 'HomeController@getClients')->name('personal-clients');
+Route::get('/home/authorized-clients', 'HomeController@getAuthorizedClients')->name('authorized-clients');
+
